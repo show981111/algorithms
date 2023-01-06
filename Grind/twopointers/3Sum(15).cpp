393e9bbba9f09b2ab1235aa7d7e471213b3952f1
show981111 <show981111@gmail.com>
2023-01-06 02:31:20 -0500

    daily leetcode

--- Grind/twopointers/3Sum(15).cpp
+++ Grind/twopointers/3Sum(15).cpp
@@ -1,44 +1,68 @@
+// can we use hashmap? -> will not remove duplicates automatically...
+// Sort and skip?
+// didnt consider duplicate between left & right
+/**
+ * Target sum => sort and use two pointers O(NlogN) OR use unordered map O(N)
+ * Key point of using two pointers is, spliting the case and make it O(N) not O(N^2)
+ *
+ * @brief Key challenge of this problem : remove duplicates
+ * two techniques removing dups
+ * 1. sort + skip redundants
+ * 2. Use set or map
+ * Usually set and map are causing an extra space overhead.
+ * If there is no TC adv, consider using sort + skip
+ *
+ * This case, using a map will be O(N^2) becuase of pivot loop * target sum loop
+ * Using a sort will O(NlogN) + O(N^2) for pivot loop * two pointers = O(N^2)
+ * Thus, using a map is not that beneficial in this case :(
+ */
 class Solution
 {
 public:
     vector<vector<int>> threeSum(vector<int> &nums)
     {
+        vector<vector<int>> ans;
+        size_t N = nums.size();
         sort(nums.begin(), nums.end());
-        int N = nums.size();
-        vector<vector<int>> res;
-        for (int pivot = 0; pivot <= N - 3; pivot++)
+
+        for (size_t pivotIndex = 0; pivotIndex < N - 2; pivotIndex++)
         {
-            if (pivot > 0 && nums[pivot] == nums[pivot - 1])
-                continue; // same pivot makes redundant result!
-
-            int left = pivot + 1;
-            int right = N - 1;
-
+            if (pivotIndex > 0 && nums[pivotIndex] == nums[pivotIndex - 1])
+                continue;
+            int pivot = nums[pivotIndex];
+            size_t left = pivotIndex + 1;
+            size_t right = N - 1;
             while (left < right)
             {
-                int s = nums[pivot] + nums[left] + nums[right];
-                if (s > 0)
+                int rightNum = nums[right];
+                int leftNum = nums[left];
+                int sum = pivot + leftNum + rightNum;
+                if (sum > 0)
                 {
-                    --right; // sum is too big
+                    // don't need this while because sum > 0, so won't affect the answer
+                    while (left < right && nums[right] == rightNum)
+                        right--;
                 }
-                else if (s < 0)
+                else if (sum < 0)
                 {
-                    ++left; // sum is too small
+                    // don't need this while too
+                    while (left < right && nums[left] == leftNum)
+                        left++;
                 }
                 else
-                { // s == 0
-                    res.push_back({nums[pivot], nums[left], nums[right]});
-                    int leftNum = nums[left];
-                    int rightNum = nums[right];
-
-                    // skip redundant elems
-                    while (left < N && nums[left] == leftNum)
-                        left++;
-                    while (right >= 0 && nums[right] == rightNum)
+                {
+                    vector<int> cand = {pivot, nums[left], nums[right]};
+                    ans.push_back(cand);
+                    // We need these while bc it affect the answer!
+                    //  because sum == 0
+                    while (left < right && nums[right] == rightNum)
                         right--;
+                    while (left < right && nums[left] == leftNum)
+                        left++;
                 }
             }
         }
-        return res;
+
+        return ans;
     }
 };
