ecb8e3a1654910604d76a1340ed943e0c4f9984a
show981111 <show981111@gmail.com>
2023-01-11 00:06:32 -0500

    daily leetcode

--- Grind/twopointers/5)longestPalidrome.cpp
+++ Grind/twopointers/5)longestPalidrome.cpp
@@ -1,3 +1,12 @@
+/*
+    Intuition for Two Pointers.
+    How do we know if a string is palindrome or not?
+    -> use two pointers from the beginning and the end.
+    OR from the middle, go to the beginning and the end => O(N)
+    Since DP is O(N^2), can we just use this method per each character?
+    YES!
+*/
+
 /**
  * @brief For each mid, we push left and right to the farthest!
  * We first go to the point where mid != left, mid != right
@@ -22,41 +31,90 @@
  * both O(N^2)
  *
  */
-
 class Solution
 {
 public:
     string longestPalindrome(string s)
     {
-        int mid = 0;
-        int left = mid, right = mid;
-        int ans = 0;
-        int maxLeft = 0;
-        while (mid < s.size() - ans / 2)
+        int N = s.size();
+        int maxStart = 0;
+        int maxLeng = 0;
+        for (int mid = 0; mid < N; mid++)
         {
-            // mid < s.size() - ans
-            // if we do this, then we will miss the possible solution because
-            // for each mid, the max length could be (end - mid) * 2
+            if (mid > 0 && s[mid] == s[mid - 1]) // no need to calculate cuase we count
+                continue;                        // the redundant mid before
+
+            int left = mid, right = mid;
             while (left >= 0 && s[mid] == s[left])
                 left--;
-            while (right < s.size() && s[right] == s[mid])
+            while (right < N && s[mid] == s[right]) // push further until it pass the redundant mid
                 right++;
-            while (left >= 0 && right < s.size() && s[left] == s[right])
+            while (left >= 0 && right < N && s[left] == s[right]) // push further until it is a palindrome
             {
                 left--;
                 right++;
-            }
-            // cout << left << " " << right << endl;
-            if (ans < right - left - 1)
-            {
-                maxLeft = left + 1;
-                ans = right - left - 1;
-            }
+            } // left +1 ~ right -1 is palindrome
 
-            mid++;
-            left = mid;
-            right = mid;
+            if (right - left - 1 > maxLeng)
+            {
+                maxLeng = right - left - 1;
+                maxStart = left + 1;
+            }
         }
-        return s.substr(maxLeft, ans);
+        return s.substr(maxStart, maxLeng);
+    }
+};
+
+/**
+ * @brief Using DP
+ *
+ */
+
+// palindrome -> famous DP problem
+/* T(N,M) = T(N+1, M-1) && a[N] == a[M]
+   [] b a b a d
+[] T. F F F F F
+b. F. T F T
+a  F. X T F
+b. F. X X T
+a  F. X X X T
+d. F  X X X X T
+
+The direction of iteration is important. Since u need (N+1, M-1) entry,
+I need to traverse from down to up so that (N+1, M-1) entry is calculated before (N,M)
+(0,0) -> (1,1) -> (1,0) -> (2,2) -> (2,1) -> (2,0) ...
+         [for each col, row is going up!]
+*/
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int N = s.size();
+        vector<vector<bool>> memo(N + 1, vector<bool>(N + 1, false));
+        memo[0][0] = true;
+        int maxLeng = 0;
+        int maxStart = 0;
+
+        for (int col = 1; col < N + 1; col++)
+        {
+            for (int row = col; row >= 1; row--)
+            {
+                if (row + 1 <= col - 1)
+                {
+                    memo[row][col] = memo[row + 1][col - 1] && (s[row - 1] == s[col - 1]);
+                }
+                else
+                {
+                    memo[row][col] = s[row - 1] == s[col - 1];
+                }
+                if (memo[row][col] && col - row + 1 > maxLeng)
+                {
+                    maxLeng = col - row + 1;
+                    maxStart = row - 1;
+                }
+            }
+        }
+        return s.substr(maxStart, maxLeng);
     }
 };
